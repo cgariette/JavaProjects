747516699420faeda02c52af7b3dedfe51c76d3d
cgariette <clifftonafande@gmail.com>
2023-02-10 12:18:28 +0300

    operators in java

---
+++ OperatorsInJAva/ArithmeticDemo.java
@@ -0,0 +1,15 @@
+public class ArithmeticDemo{
+	public static void main (String args []){
+		int sum = 1+2;
+		int sub = 7-5;
+		int mul= sub*sum;
+		int div = mul/sub;
+		
+		System.out.println("The sum is : " + sum);
+		System.out.println("The subtraction is : " + sub);
+		System.out.println("The multiplication is : " + mul);
+		System.out.println("The division is : " + div);
+
+	}
+}
+		
