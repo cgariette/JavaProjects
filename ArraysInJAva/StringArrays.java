495ab094df9542b637e505ab3343b66cbcdc4be9
cgariette <clifftonafande@gmail.com>
2023-02-10 19:15:49 +0300

    String arrays

---
+++ ArraysInJAva/StringArrays.java
@@ -0,0 +1,16 @@
+public class StringArrays {
+	public static void main(String args []){
+		String[] arr;
+
+		arr = new String[6];
+
+		arr[0] = "Cliff";
+		arr[1] = "Liny";
+		arr[2] = "Puri";
+		arr[3] = "Dai";
+		arr[4] = "Charles";
+		arr[5] = "Conny";
+
+		System.out.println(arr[0] + " is the first born. " + arr[1] + " is the second born. " + arr[2] + " is the third born. " + arr[3] + " is the last born. " + arr[4] + " is their dad and " + arr[5] + " is their mom.");
+	}
+}
