777a80895fa9a697c4b582f77a0598ba1af7694d
cgariette <clifftonafande@gmail.com>
2023-02-15 15:32:15 +0300

    added input rows on pyramid

--- Pyramid/Pyramid.java
+++ Pyramid/Pyramid.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pyramid{
 	public static void main (String args []){
-		int rows =13;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Please enter the number of rows: ");
+		
+		int rows= scan.nextInt();
 
 		for (int i=0; i<rows; i++){
 			for (int j=0; j<rows-i; j++) {
