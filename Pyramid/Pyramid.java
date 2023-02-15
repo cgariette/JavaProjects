16e8bce47a905c18b0f6b11750e7b2e43c219cf9
cgariette <clifftonafande@gmail.com>
2023-02-15 15:16:23 +0300

    Fixed the pyramid

--- Pyramid/Pyramid.java
+++ Pyramid/Pyramid.java
@@ -1,13 +1,13 @@
 public class Pyramid{
 	public static void main (String args []){
-		int rows =5;
+		int rows =13;
 
 		for (int i=0; i<rows; i++){
 			for (int j=0; j<rows-i; j++) {
-				System.out.println(" ");
+				System.out.print(" ");
 			}
 	for (int k=0; k<=i; k++) {
-		System.out.println("* ");
+		System.out.print("* ");
 	}
 	System.out.println();
 		}
