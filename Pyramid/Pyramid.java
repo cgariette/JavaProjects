a06ff92ab81e7538d0f0dcbb18f053bf4a7fd357
cgariette <clifftonafande@gmail.com>
2023-02-15 08:03:25 +0300

    -i

--- Pyramid/Pyramid.java
+++ Pyramid/Pyramid.java
@@ -1,9 +1,9 @@
 public class Pyramid{
 	public static void main (String args []){
-		int rows =9;
+		int rows =5;
 
 		for (int i=0; i<rows; i++){
-			for (int j=0; j <rows-1; j++) {
+			for (int j=0; j<rows-i; j++) {
 				System.out.println(" ");
 			}
 	for (int k=0; k<=i; k++) {
